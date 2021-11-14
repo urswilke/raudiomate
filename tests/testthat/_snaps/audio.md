2d5e5758c5ab66d306bb5e4e57a1f036919c175a
Urs Wilke <urs.wilke@gmail.com>
2021-11-14 22:05:06 +0100

    don't test snapshot for fluidsynth stdout

--- tests/testthat/_snaps/audio.md
+++ tests/testthat/_snaps/audio.md
@@ -7,16 +7,3 @@
         <source src="test.wav" type="audio/wav"/>
       </audio>
 
-# output of synthesized wav file & mp3 conversion correct
-
-    FluidSynth runtime version 2.1.1
-    Copyright (C) 2000-2020 Peter Hanappe and others.
-    Distributed under the LGPL license.
-    SoundFont(R) is a registered trademark of E-mu Systems, Inc.
-    
-    Rendering audio to file 'test.wav'..
-
----
-
-    
-
--- tests/testthat/test-audio.R
+++ tests/testthat/test-audio.R
@@ -11,18 +11,19 @@ test_that("html tag output works", {
 test_that("output of synthesized wav file & mp3 conversion correct", {
 
   withr::with_file(c(wavfile, mp3file), {
-    testthat::expect_snapshot_output({
-      # testthat::skip_on_ci()
-      synthesize_midi(midifile, wavfile, verbose = TRUE)
-    })
-    testthat::expect_snapshot_output({
-      expect_success(
-        expect_null(
-          synthesize_midi(midifile, wavfile)
-        )
+    expect_success(
+      expect_null(
+        # testthat::skip_on_ci()
+        synthesize_midi(midifile, wavfile, verbose = TRUE)
       )
+    )
 
-    })
+    expect_success(
+      expect_null(
+        synthesize_midi(midifile, wavfile)
+      )
+    )
+
 
     testthat::expect_success({
       # null means the function didn't throw an error:
