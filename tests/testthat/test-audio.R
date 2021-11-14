b41dbe24d8c7275bc4459dc185ad071e3f099ee1
Urs Wilke <urs.wilke@gmail.com>
2021-11-14 21:18:37 +0100

    remove test for verbose output, as it changes even on my machine

--- tests/testthat/test-audio.R
+++ tests/testthat/test-audio.R
@@ -1,4 +1,4 @@
-midifile <- system.file("extdata", "test_midi_file.mid", package = "pyramidi")
+midifile <- system.file("extdata", "test_midi_file.mid", package = "raudiomate")
 wavfile <- "test.wav"
 mp3file <- "test.mp3"
 
@@ -12,6 +12,6 @@ test_that("output of synthesized wav file & mp3 conversion correct", {
   testthat::skip_on_ci()
   withr::with_file(c(wavfile, mp3file), {
     testthat::expect_snapshot_output(synthesize_midi(midifile, wavfile, verbose = TRUE))
-    testthat::expect_snapshot_output(convert_to_mp3(wavfile, verbose = TRUE))
+    testthat::expect_success(expect_null(convert_to_mp3(wavfile)))
   })
 })
